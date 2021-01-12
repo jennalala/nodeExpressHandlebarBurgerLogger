5a2c6a77a0b0257b7dfd532493796c2e3e0aaa60
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2021-01-12 14:06:37 -0500

    update js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,56 +1,35 @@
-$(function() {
-    $(".change-eaten").on("click", function(event) {
-      var id = $(this).data("id");
-      var newEat = $(this).data("devour");
+$(function () {
+    $(".change-devoured").on("click", function (event) {
+      const id = $(this).data("id");
+      const newDevoured = $(this).data("newdevour");
   
-      var newEatState = {
-        isEaten: newEat
+      const newDevourState = {
+        devoured: newDevoured,
       };
   
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: newEatState
-      }).then(
-        function() {
-          console.log("changed eaten to", newEat);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+        data: newDevourState,
+      }).then(function () {
+        // Reload the page to get the updated list
+        location.reload();
+      });
     });
   
-    $(".create-form").on("submit", function(event) {
+    $(".create-form").on("submit", function (event) {
+  
       event.preventDefault();
   
-      var newBurger = {
-        name: $("#burgers").val().trim(),
-        isEaten: $("[name=isEaten]:checked").val().trim()
+      let newBurger = {
+        name: $("#burger").val().trim(),
       };
-  
+      // Send the POST request.
       $.ajax("/api/burgers", {
         type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          
-          location.reload();
-        }
-      );
-    });
-  
-    $(".delete-burger").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/burgers/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted burger", id);
-
-          location.reload();
-        }
-      );
+        data: newBurger,
+      }).then(function () {
+       
+        location.reload();
+      });
     });
   });
