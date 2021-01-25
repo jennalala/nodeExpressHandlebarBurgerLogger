1cfca27f224c6b8527286372179213c5394eac90
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2021-01-25 12:03:06 -0500

    moved files, trying to get deployed site working

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- testing db
+INSERT INTO burgers (burger_name) VALUES ("Munster Cheese");
+INSERT INTO burgers (burger_name, devoured) VALUES ("McDouble", true);
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
